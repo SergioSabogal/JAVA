1e1bdfd166ed7dd2eca10a8ef5473f8b74723a20
Sergio Sabogal <sergiosabogal1911@gmail.com>
2026-02-23 16:02:01 -0500

    Implementaciónd e hilos java 8-2

--- src/org/ssabogal/udemy/hilos/ejemplos/EjemploRunnableJava8.java
+++ src/org/ssabogal/udemy/hilos/ejemplos/EjemploRunnableJava8.java
@@ -1,7 +1,7 @@
 package org.ssabogal.udemy.hilos.ejemplos;
 
 public class EjemploRunnableJava8 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Runnable hilo = () -> {
             for (int i = 1; i <= 10; i++) {
@@ -13,8 +13,18 @@ public class EjemploRunnableJava8 {
             System.out.println("Ultima tarea :  " + Thread.currentThread().getName());
         };
 
-        new Thread(hilo , "Backup").start();
-        new Thread(hilo , "Restore").start();
+       Thread h1 =  new Thread(hilo , " Backup");
+       Thread h2 = new Thread(hilo , " Restore");
+        Thread h3 = new Thread(hilo, " Actualización");
 
-    }
+        h1.start();
+        h2.start();
+        h3.start();
+
+        h1.join();
+        h2.join();
+        h3.join();
+
+        System.out.println("Continuando con la ejecución del metodo");
+     }
 }
