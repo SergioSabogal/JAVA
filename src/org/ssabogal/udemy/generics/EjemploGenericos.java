fd6359125ec72358dba87ea8083da9d1ebfa641d
Sergio Sabogal <sergiosabogal1911@gmail.com>
2026-04-07 16:12:28 -0500

    Commit

---
+++ src/org/ssabogal/udemy/generics/EjemploGenericos.java
@@ -0,0 +1,14 @@
+package org.ssabogal.udemy.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EjemploGenericos {
+    public static void main(String[] args) {
+
+        List clientes = new ArrayList();
+    }
+
+
+    public static
+}
